00cedb78da51b395337057ab68e2c97160246de7
Oleg <nanobyte77@mail.ru>
2022-11-14 13:58:13 +0900

    Задача про перебор чисел

---
+++ Task04/Program.cs
@@ -0,0 +1,10 @@
+﻿// Выводим числа от -N до N
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = -number;
+while (count <= number)
+{
+    Console.Write($"{count} ");
+    count++;
+}
