ea0286b9abae32d02b0249b97ffe4690d8840b85
Oleg <nanobyte77@mail.ru>
2022-11-14 12:55:14 +0900

    Программа про квадраты

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b*b) 
+{
+ Console.WriteLine("Да");   
+}
+else
+{
+Console.WriteLine("Нет"); 
+}
