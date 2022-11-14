7093c626e0611dc5df33b3e765399b0cccc865b6
Oleg <nanobyte77@mail.ru>
2022-11-14 13:27:41 +0900

    Дополнения

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,10 +1,11 @@
-﻿Console.WriteLine("Введите число: ");
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
-if (a == 1) Console.WriteLine("Понедельник"); 
-if (a == 2) Console.WriteLine("Вторник");
-if (a == 3) Console.WriteLine("Среда");
-if (a == 4) Console.WriteLine("Четверг");
-if (a == 5) Console.WriteLine("Пятница");
-if (a == 6) Console.WriteLine("Суббота");
-if (a == 7) Console.WriteLine("Воскресенье");
+if (a == 1) Console.WriteLine("Понедельник");
+else if (a == 2) Console.WriteLine("Вторник");
+else if (a == 3) Console.WriteLine("Среда");
+else if (a == 4) Console.WriteLine("Четверг");
+else if (a == 5) Console.WriteLine("Пятница");
+else if (a == 6) Console.WriteLine("Суббота");
+else if (a == 7) Console.WriteLine("Воскресенье");
