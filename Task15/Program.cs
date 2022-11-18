6e2ff5d1de384747e73352c008c335813a837871
Oleg <nanobyte77@mail.ru>
2022-11-18 13:25:52 +0900

    Решение задачи 15 про дни недели

---
+++ Task15/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Start: Console.WriteLine("Введите число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1 || number > 7)
+{
+    Console.WriteLine("Введено неверное число");
+    goto Start;
+}
+
+string[] array = { "нет", "нет", "нет", "нет", "нет", "да", "да" };
+Console.WriteLine(array[number - 1]);
