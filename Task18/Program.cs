0e75081e79e1d63b7336ab8cc7ce51602bd985f6
Oleg <nanobyte77@mail.ru>
2022-11-22 13:33:49 +0900

    Работа на семинаре

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -1,13 +1,13 @@
 ﻿Console.Clear();
 Console.Write("Введите номер четверти: ");
-int n = Convert.ToInt32(Console.ReadLine());
+string n = Console.ReadLine();
 
-string Quarter(int nc)
+string Quarter(string nc)
 {
-    if (nc == 1) return "x > 0 и y > 0";
-    if (nc == 2) return "x < 0 и y < 0";
-    if (nc == 3) return "x < 0 и y > 0";
-    if (nc == 4) return "x > 0 и y < 0";
+    if (nc == "1") return "x > 0 и y > 0";
+    if (nc == "2") return "x < 0 и y < 0";
+    if (nc == "3") return "x < 0 и y > 0";
+    if (nc == "4") return "x > 0 и y < 0";
     return "Введены некорректные данные";
 }
 
---
+++ Task20/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.WriteLine("Введите координаты точки А");
+Console.Write("Введите координату Х: ");
+double xa = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y: ");
+double ya = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B");
+Console.Write("Введите координату Х: ");
+double xb = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y: ");
+double yb = Convert.ToDouble(Console.ReadLine());
+
+double Distance (double xa, double ya, double xb, double yb)
+{
+    double xRas = (xa > xb) ? xRas = xa - xb : xRas = xb - xa;
+    double yRas = (ya > yb) ? yRas = ya - yb : yRas = yb - ya;
+    double res = Math.Sqrt(xRas*xRas + yRas*yRas);
+    return res;
+}
+
+double result2 = Math.Round((Distance (xa, ya, xb, yb)), 2, MidpointRounding.ToZero);
+Console.WriteLine(result2);
