b5348c6a1ce972ea24aea61985e58342274293ad
Oleg <nanobyte77@mail.ru>
2022-11-22 16:55:35 +0900

    Работа на семинаре

--- Task24/Program.cs
+++ Task24/Program.cs
@@ -7,15 +7,19 @@
 System.Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int SumDigits (int num)
+int SumDigits(int num)
 {
     int sum = default;
-    for (int i = 0; i <= num; i++)
+    for (int i = 1; i <= num; i++)
     {
         sum = sum + i;
     }
     return sum;
 }
 
-int sumDigit = SumDigits(number);
-System.Console.WriteLine($"Сумма чисел от 1 до {number} равно {sumDigit}");
+if (number > 0)
+{
+    int sumDigit = SumDigits(number);
+    System.Console.WriteLine($"Сумма чисел от 1 до {number} равно {sumDigit}");
+}
+else System.Console.WriteLine("Требуется ввести натуральное число");
