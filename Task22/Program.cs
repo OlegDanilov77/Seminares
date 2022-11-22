1a89ad1431aeef6da0df3338994fd78d93f62466
Oleg <nanobyte77@mail.ru>
2022-11-22 16:48:25 +0900

    Работа на семинаре

---
+++ Task22/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число N");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void Square (int number)
+{
+   for (int i = 1; i <= number; i++)
+   {
+        Console.WriteLine($"{i} и его квадрат {i*i}");
+   }
+}
+
+Square(num);
---
+++ Task24/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits (int num)
+{
+    int sum = default;
+    for (int i = 0; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+int sumDigit = SumDigits(number);
+System.Console.WriteLine($"Сумма чисел от 1 до {number} равно {sumDigit}");
