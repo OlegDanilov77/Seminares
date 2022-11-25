5e505e316f7a7ac5d5276ec34b873d7c0381554f
Oleg <nanobyte77@mail.ru>
2022-11-25 15:53:53 +0900

    Работа на семинаре

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -1,15 +1,3 @@
 ﻿Console.Clear();
 Console.Write("Введите номер четверти: ");
-string n = Console.ReadLine();
 
-string Quarter(string nc)
-{
-    if (nc == "1") return "x > 0 и y > 0";
-    if (nc == "2") return "x < 0 и y < 0";
-    if (nc == "3") return "x < 0 и y > 0";
-    if (nc == "4") return "x > 0 и y < 0";
-    return "Введены некорректные данные";
-}
-
-string quarter = Quarter(n);
-Console.WriteLine(quarter);
