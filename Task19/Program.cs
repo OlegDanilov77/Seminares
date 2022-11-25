8ca73167df21cacdd29805f711843ef4eb2de86b
Oleg <nanobyte77@mail.ru>
2022-11-25 15:47:18 +0900

    Решение задачи 19 про палиндром

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -6,14 +6,14 @@
 System.Console.WriteLine("Введите пятизначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if (num>9999 && num<100000)
+if (num > 9999 && num < 100000)
 {
     int num1 = num / 10000;
     int num2 = (num / 1000) % 10;
     int num4 = (num % 100) / 10;
     int num5 = num % 10;
-    
-    if (num1 == num5 && num2 == num4) 
+
+    if (num1 == num5 && num2 == num4)
     {
         System.Console.WriteLine("да");
     }
