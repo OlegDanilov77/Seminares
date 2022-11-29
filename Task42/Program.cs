6518819e0c3d79616581c133f2d3281d1c8eed0c
Oleg <nanobyte77@mail.ru>
2022-11-29 17:45:44 +0900

    Работа на семинаре

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -14,3 +14,30 @@ string ConvertToBin(int num)
 
 string newNumberBin = ConvertToBin(number);
 Console.WriteLine($"Преобразованное двоичное число равно {newNumberBin}");
+
+// void PrintArray(int[] arr, string 
+// int[] Binary(int numb)
+// {
+//     int numb1 = numb;
+//     int digits = 0;
+//     while (numb1 > 0)
+//     {
+//         numb1 /= 2;
+//         digits += 1;
+
+//     }
+//     int[] bin = new int[digits];
+//     for (int i = 0; i < bin.Length; i++)
+//     {
+//         bin[bin.Length-i-1] = numb % 2;
+//         numb /= 2;
+//     }
+//     return bin;
+// }
+
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int[] binary = Binary(number);
+// // Console.WriteLine(Binary(number));
+// PrintArray(binary, "", "", "");
