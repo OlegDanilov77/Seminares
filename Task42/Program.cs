9a199dca73175ea10d80440d3e3116f5e95180f6
Oleg <nanobyte77@mail.ru>
2022-11-29 17:12:25 +0900

    работа на семинаре

---
+++ Task42/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите десятичное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string ConvertToBin(int num)
+{
+    string str = string.Empty;
+    while (num > 0)
+    {
+        str = num % 2 + str;
+        num = num / 2;
+    }
+    return str;
+}
+
+string newNumberBin = ConvertToBin(number);
+Console.WriteLine($"Преобразованное двоичное число равно {newNumberBin}");
